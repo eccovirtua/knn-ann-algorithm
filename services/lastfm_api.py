d46d57c84dcfaf9c68191f7bd82667fade4ff7e3
Martin M. <drekgx@gmail.com>
2025-09-11 23:53:17 -0300

    0.2.1

--- services/lastfm_api.py
+++ services/lastfm_api.py
@@ -1,14 +1,11 @@
 # services/lastfm_api.py
 import httpx
+# import requests
 
 LASTFM_API_KEY = "35cb090bb75bc5d49c342c37d0d5e625"
 LASTFM_BASE_URL = "https://ws.audioscrobbler.com/2.0/"
 
 async def get_album_art(artist: str, track: str) -> str | None:
-    """
-    Busca la portada de un track en Last.fm usando artista + track.
-    Retorna la URL de la imagen o None si no hay resultados.
-    """
     if not LASTFM_API_KEY:
         return None
 
@@ -26,13 +23,25 @@ async def get_album_art(artist: str, track: str) -> str | None:
             resp.raise_for_status()
             data = resp.json()
 
-            # Navegamos en la respuesta
-            image_list = data.get("track", {}).get("album", {}).get("image", [])
-            if image_list:
-                # Tomamos la última (generalmente la de mayor resolución)
-                return image_list[-1].get("#text")
+            album = data.get("track", {}).get("album")
+            if not album:
+                return None  # si no hay album, no hay portada
+
+            image_list = album.get("image", [])
+            for img in reversed(image_list):  # revisa de mayor a menor tamaño
+                url = img.get("#text")
+                if url and "2a96cbd8b46e442fc41c2b86b821562f.png" not in url:
+                    return url
 
     except Exception as e:
         print(f"Error Last.fm: {e}")
 
     return None
+
+# API_KEY = LASTFM_API_KEY
+# artist = "Coldplay"
+# track = "Yellow"
+#
+# url = f"http://ws.audioscrobbler.com/2.0/?method=track.getInfo&api_key={API_KEY}&artist={artist}&track={track}&format=json"
+# resp = requests.get(url)
+# print(resp.json())
